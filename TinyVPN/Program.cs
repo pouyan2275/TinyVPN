0a8acc367be375ed8637d4fb96a9442e964e9af0
pouyan alizadeh <pouyan9913@gmail.com>
2024-05-19 16:23:41 +0330

    سعی کردم یکسری متد بسازم و کد اصلی رو داخل اونا قرار بدم و یکسری لاگ بیشتر به برنامه اضافه شد تا کاربر کاملا متوجه بشه دقیقا برنامه توی چه مرحله ای هست

--- TinyVPN/Program.cs
+++ TinyVPN/Program.cs
@@ -1,41 +1,33 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Pouyan;
 using SingBoxLib.Configuration.Outbound.Abstract;
+using SingBoxLib.Parsing;
 
 var builder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json");
 var configuration = builder.Build();
-
 var profileTake = int.Parse(configuration["count_profile"]!);
 var url = "https://raw.githubusercontent.com/Mahdi0024/ProxyCollector/master/sub/proxies.txt";
-
-static void OnProcessExit(CancellationTokenSource cts)
-{
-    cts.Cancel();
-    Network.DisableProxy();
-}
-
 var singbox = new SingBox("./sing-box.exe", Pouyan.Model.Inbounds.Mixed);
 var cts = new CancellationTokenSource();
-
-var profiles = SingBox.GetProfilesFromSubscribe(url);
-
-Console.WriteLine($"{profiles.Count} Profiles Founded ");
-Console.WriteLine($"Select {profileTake} Profiles From {profiles.Count} Profiles");
-
+var profiles = TakeProfiles();
 var freePorts = Network.GetFreePorts(profileTake + 1);
 int i = 0;
 var profilesResults = new List<Task<Pouyan.Model.TestResult>>();
+if(profileTake > profiles.Count)
+    profileTake = profiles.Count;
 
 Console.WriteLine($"Testing Profiles");
 profiles.GetRange(0, profileTake).ForEach(p =>
 {
     i = i + 1;
+    Console.WriteLine($"Start Test {i}.{p.Name}");
     profilesResults.Add(singbox.UrlTestAsync(p, freePorts[i]));
 });
 
 var testedProfiles = Task.WhenAll(profilesResults).Result;
+testedProfiles.ToList().ForEach(p => Console.WriteLine($"{p.Profile.Name} - Delay:{p.Result!.Delay}"));
 var orderedProfiles = testedProfiles.Where(p => p.Result!.Delay > 0).OrderBy(p => p.Result!.Delay).ToList();
 if (!orderedProfiles.Any())
 {
@@ -54,3 +46,16 @@ Console.CancelKeyPress += new ConsoleCancelEventHandler((e, s) => OnProcessExit(
 AppDomain.CurrentDomain.ProcessExit += new EventHandler((s, e) => OnProcessExit(cts));
 tunneling.Wait();
 
+List<ProfileItem> TakeProfiles()
+{
+    var profiles = SingBox.GetProfilesFromSubscribe(url);
+
+    Console.WriteLine($"{profiles.Count} Profiles Founded ");
+    Console.WriteLine($"Select {profileTake} Profiles From {profiles.Count} Profiles");
+    return profiles;
+}
+static void OnProcessExit(CancellationTokenSource cts)
+{
+    cts.Cancel();
+    Network.DisableProxy();
+}
