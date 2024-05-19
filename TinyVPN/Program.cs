5e4dc9b86ccd34c9c97584450c41d1a07e2b491e
pouyan alizadeh <pouyan9913@gmail.com>
2024-05-19 14:23:58 +0330

    fix some warnings

--- TinyVPN/Program.cs
+++ TinyVPN/Program.cs
@@ -1,14 +1,13 @@
-﻿using Pouyan;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Pouyan;
 using SingBoxLib.Configuration.Outbound.Abstract;
-using System.Diagnostics;
 
 var builder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json");
 var configuration = builder.Build();
 
-var profileTake = int.Parse(configuration["count_profile"]);
+var profileTake = int.Parse(configuration["count_profile"]!);
 var url = "https://raw.githubusercontent.com/Mahdi0024/ProxyCollector/master/sub/proxies.txt";
 
 static void OnProcessExit(CancellationTokenSource cts)
@@ -17,7 +16,7 @@ static void OnProcessExit(CancellationTokenSource cts)
     Network.DisableProxy();
 }
 
-var singbox = new SingBox("./sing-box.exe",Pouyan.Model.Inbounds.Mixed);
+var singbox = new SingBox("./sing-box.exe", Pouyan.Model.Inbounds.Mixed);
 var cts = new CancellationTokenSource();
 
 var profiles = SingBox.GetProfilesFromSubscribe(url);
@@ -25,27 +24,28 @@ var profiles = SingBox.GetProfilesFromSubscribe(url);
 Console.WriteLine($"{profiles.Count} Profiles Founded");
 Console.WriteLine($"Select {profileTake} Profiles From {profiles.Count} Profiles");
 
-var freePorts = Network.GetFreePorts(profileTake+1);
+var freePorts = Network.GetFreePorts(profileTake + 1);
 int i = 0;
 var profilesResults = new List<Task<Pouyan.Model.TestResult>>();
 
 Console.WriteLine($"Testing Profiles");
-profiles.GetRange(0,profileTake).ForEach(p => {
+profiles.GetRange(0, profileTake).ForEach(p =>
+{
     i = i + 1;
     profilesResults.Add(singbox.UrlTestAsync(p, freePorts[i]));
-    });
+});
 
 var testedProfiles = Task.WhenAll(profilesResults).Result;
-var orderedProfiles = testedProfiles.Where(p => p.Result.Delay > 0).OrderBy(p=>p.Result.Delay).ToList();
+var orderedProfiles = testedProfiles.Where(p => p.Result!.Delay > 0).OrderBy(p => p.Result!.Delay).ToList();
 if (!orderedProfiles.Any())
 {
     Console.WriteLine("0 profiles work well with your internet connection");
     return;
 }
 Console.WriteLine("Profiles Work Well:");
-orderedProfiles.ForEach(p => { Console.WriteLine($"Name: {p.Profile.Name} Delay: {p.Result.Delay}"); });
+orderedProfiles.ForEach(p => { Console.WriteLine($"Name: {p.Profile.Name} Delay: {p.Result!.Delay}"); });
 
-singbox.OutBounds = new List<OutboundConfig> { orderedProfiles[0].Profile.ToOutboundConfig() } ;
+singbox.OutBounds = new List<OutboundConfig> { orderedProfiles[0].Profile.ToOutboundConfig() };
 Console.WriteLine($"Connecting To {orderedProfiles[0].Profile.Name}");
 var tunneling = singbox.StartTunneling(cts);
 Console.WriteLine($"Connected");
