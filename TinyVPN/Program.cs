fcaa73bc6918ca6de9967ad0e4383b2bc8c5564a
pouyan alizadeh <pouyan9913@gmail.com>
2024-05-20 12:30:02 +0330

    1. اضافه شدن url به فایل appsettings.json 2. تلاش برای اتصال به همه ی کانفیگ ها از طریق جدا کننده ی count_profile

--- TinyVPN/Program.cs
+++ TinyVPN/Program.cs
@@ -3,34 +3,45 @@ using Pouyan;
 using SingBoxLib.Configuration.Outbound.Abstract;
 using SingBoxLib.Parsing;
 
+//variables
+Pouyan.Model.TestResult[] testedProfiles;
+List<Task<Pouyan.Model.TestResult>> profilesResults;
+List<ProfileItem> profiles;
+int index,counterProfile;
+string url;
+List<Pouyan.Model.TestResult> orderedProfiles;
+//variables
+
 var builder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json");
 var configuration = builder.Build();
 
-var profileTake = int.Parse(configuration["count_profile"]!);
-
-var url = "https://raw.githubusercontent.com/Mahdi0024/ProxyCollector/master/sub/proxies.txt";
+counterProfile = int.Parse(configuration["count_profile"]!);
+url = configuration["subscribe_url"]!;
 
 var singbox = new SingBox("./sing-box.exe", Pouyan.Model.Inbounds.Mixed);
 
 var cts = new CancellationTokenSource();
 
-var profiles = TakeProfiles();
-if (profileTake > profiles.Count)
-    profileTake = profiles.Count;
+profiles = TakeProfiles();
+if (counterProfile > profiles.Count)
+    counterProfile = profiles.Count;
+index = 0;
+Console.WriteLine($"doing {counterProfile} test profiles");
+do
+{
+    profilesResults = TestProfiles(index, counterProfile, singbox, profiles);
+    testedProfiles = CheckProfiles(profilesResults);
+    orderedProfiles = testedProfiles.Where(p => p.Result!.Delay > 0).OrderBy(p => p.Result!.Delay).ToList();
+    index += counterProfile;
+    if (index > profiles.Count)
+        index = profiles.Count;
+} while (orderedProfiles.Count == 0 || index == profiles.Count);
 
-var profilesResults = new List<Task<Pouyan.Model.TestResult>>();
-TestProfiles(profileTake,singbox,profiles,profilesResults);
-
-var testedProfiles = Task.WhenAll(profilesResults).Result;
-testedProfiles.ToList().ForEach(p => Console.WriteLine($"{p.Profile.Name} - Delay:{p.Result!.Delay}"));
-
-var orderedProfiles = testedProfiles.Where(p => p.Result!.Delay > 0).OrderBy(p => p.Result!.Delay).ToList();
-if (!orderedProfiles.Any())
+if (orderedProfiles.Count == 0)
 {
     Console.WriteLine("0 profiles work well with your internet connection");
-    Console.ReadLine();
     return;
 }
 Console.WriteLine("Profiles Work Well:");
@@ -59,7 +70,6 @@ List<ProfileItem> TakeProfiles()
     var profiles = SingBox.GetProfilesFromSubscribe(url);
 
     Console.WriteLine($"{profiles.Count} Profiles Founded ");
-    Console.WriteLine($"Select {profileTake} Profiles From {profiles.Count} Profiles");
     return profiles;
 }
 static void OnProcessExit(CancellationTokenSource cts)
@@ -68,15 +78,24 @@ static void OnProcessExit(CancellationTokenSource cts)
     Network.DisableProxy();
 }
 
-void TestProfiles(int count, SingBox singbox, List<ProfileItem> profiles, List<Task<Pouyan.Model.TestResult>> testResults)
+List<Task<Pouyan.Model.TestResult>> TestProfiles(int index, int count, SingBox singbox, List<ProfileItem> profiles)
 {
-    Console.WriteLine($"Testing Profiles");
+    var testResults = new List<Task<Pouyan.Model.TestResult>>();
+    Console.WriteLine($"Start Test Profiles...");
     var freePorts = Network.GetFreePorts(count + 1);
     int i = 0;
-    profiles.GetRange(0, count).ForEach(p =>
+    profiles.GetRange(index, count).ForEach(p =>
     {
         i = i + 1;
-        Console.WriteLine($"Start Test {i}.{p.Name}");
         testResults.Add(singbox.UrlTestAsync(p, freePorts[i]));
     });
+    return testResults;
+}
+
+Pouyan.Model.TestResult[] CheckProfiles(List<Task<Pouyan.Model.TestResult>> profilesResults)
+{
+    var testedProfiles = Task.WhenAll(profilesResults).Result;
+    Console.WriteLine("Test Result:");
+    testedProfiles.ToList().ForEach(p => Console.WriteLine($"{p.Profile.Name} - Delay:{p.Result!.Delay}"));
+    return testedProfiles;
 }
