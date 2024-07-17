e00b027d133b972372bb2464e588055c58b779ab
pouyan alizadeh <pouyan9913@gmail.com>
2024-07-18 00:04:58 +0330

    add folder for singbox and close sing box on closing event

--- TinyVPN/Program.cs
+++ TinyVPN/Program.cs
@@ -1,31 +1,41 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Pouyan;
+using Pouyan.SingBox;
 using Pouyan.SingBox.Model;
 using SingBoxLib.Parsing;
 
 //variables
 List<TestResult> profilesResults;
-List<ProfileItem> profiles;
 int index, counterProfile;
 string url;
 List<TestResult> orderedProfiles;
-Vpn vpn;
+Vpn vpn = new Vpn();
+string singboxPath = "./singbox/sing-box.exe";
+var profileTester = new ProfileTester(singboxPath);
+var cts = new CancellationTokenSource();
+var inbounds = Pouyan.SingBox.Inbound.CreateMixedInbound(
+    listen:"127.0.0.1",
+    listenPort: 3080,
+    true
+    );
+Random rng = new();
 //variables
+
+
 
 var builder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json");
+
 var configuration = builder.Build();
 counterProfile = int.Parse(configuration["count_profile"]!);
 url = configuration["subscribe_url"]!;
 
-var inbounds = Pouyan.SingBox.Inbound.CreateMixedInbound();
-var singbox = new Pouyan.SingBox.Tunnel("./sing-box.exe", [inbounds]);
-vpn = new Vpn("./sing-box.exe");
-var cts = new CancellationTokenSource();
-Random rng = new();
 
-profiles = [.. vpn.TakeProfiles(url).OrderBy(x => rng.Next())];
+var singbox = new Pouyan.SingBox.Tunnel(singboxPath, [inbounds]);
+
+
+List<ProfileItem> profiles = [.. vpn.TakeProfiles(url).OrderBy(x => rng.Next())];
 
 if (counterProfile > profiles.Count)
     counterProfile = profiles.Count;
@@ -33,7 +43,8 @@ index = 0;
 Console.WriteLine($"doing {counterProfile} test profiles");
 do
 {
-    profilesResults = vpn.TestProfiles(index, counterProfile, profiles).Result.ToList();
+    Console.WriteLine($"Start Test Profiles...");
+    profilesResults = profileTester!.UrlTestAsync(profiles.GetRange(index, counterProfile)).Result.ToList();
     vpn.WriteTestResult(profilesResults);
     orderedProfiles = profilesResults.Where(p => p.Result!.Delay > 0).OrderBy(p => p.Result!.Delay).ToList();
     index += counterProfile;
@@ -44,17 +55,21 @@ do
 if (orderedProfiles.Count == 0)
 {
     Console.WriteLine("0 profiles work well with your internet connection");
+    Console.ReadLine();
     return;
 }
 Console.WriteLine("Profiles Work Well:");
 orderedProfiles.ForEach(p => { Console.WriteLine($"Name: {p.Profile.Name} Delay: {p.Result!.Delay}"); });
 
 Console.WriteLine($"Connecting To {orderedProfiles[0].Profile.Name}");
+
 var tunneling = singbox.StartAsync(orderedProfiles[0].Profile, cts, (sender , log) =>
 {
     Console.WriteLine(log);
 });
 Console.WriteLine($"Connected");
+
+
 Console.CancelKeyPress += new ConsoleCancelEventHandler((e, s) => OnProcessExit(cts));
 AppDomain.CurrentDomain.ProcessExit += new EventHandler((s, e) => OnProcessExit(cts));
 Console.ReadLine();
@@ -62,6 +77,7 @@ Console.ReadLine();
 
 static void OnProcessExit(CancellationTokenSource cts)
 {
+    Pouyan.SingBox.Tunnel.CloseSingBox();
     cts.Cancel();
     Pouyan.Network.Proxy.DisableProxy();
 }
--- TinyVPN/Vpn.cs
+++ TinyVPN/Vpn.cs
@@ -4,18 +4,7 @@ using SingBoxLib.Parsing;
 namespace Pouyan
 {
     public class Vpn
-    {
-        private static SingBox.ProfileTester? _testConfig;
-        public Vpn(string singBoxExecutablePath)
-        {
-            _testConfig = new SingBox.ProfileTester(singBoxExecutablePath);
-        }
-        public async Task<IEnumerable<TestResult>> TestProfiles(int index, int count, List<ProfileItem> profiles)
-        {
-            Console.WriteLine($"Start Test Profiles...");
-            return await _testConfig!.UrlTestAsync(profiles.GetRange(index, count));
-        }
-        
+    { 
         public void WriteTestResult(List<TestResult> profilesResults)
         {
             Console.WriteLine("Test Result:");
